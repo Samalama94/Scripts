b595ccd12e61666a0a35b50f8ce6fde1f1009428
Samalama94 <samsell2021@outlook.com>
2023-08-17 02:10:04 -0400

    Changed ore tracker, still need to test

--- OreTracker/Program.cs
+++ OreTracker/Program.cs
@@ -1,51 +1,16 @@
-﻿using Sandbox.Game.EntityComponents;
-using Sandbox.ModAPI.Ingame;
-using Sandbox.ModAPI.Interfaces;
-using SpaceEngineers.Game.ModAPI.Ingame;
+﻿using Sandbox.ModAPI.Ingame;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
 using System.Text;
-using VRage;
-using VRage.Collections;
-using VRage.Game;
-using VRage.Game.Components;
 using VRage.Game.GUI.TextPanel;
 using VRage.Game.ModAPI.Ingame;
-using VRage.Game.ModAPI.Ingame.Utilities;
-using VRage.Game.ObjectBuilders.Definitions;
-using VRageMath;
 
 namespace IngameScript
 {
-    partial class Program : MyGridProgram
+    internal partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
-
-
-
-
+        private IMyCockpit Cockpit;
+        private string CockpitName;
 
         public Program()
         {
@@ -53,24 +18,30 @@ namespace IngameScript
         }
 
         public void Save()
+        { }
+
+        private void Main(string argument)
         {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
+            if (argument == null) return;
+            CockpitName = argument;
+
+            Cockpit = GridTerminalSystem.GetBlockWithName(CockpitName) as IMyCockpit;
+            if (Cockpit == null) return;
+
+            DisplayFuel(Cockpit.GetSurface(2));
+            DisplayOres(Cockpit.GetSurface(0));
+            DisplayCargoFillLevel(Cockpit.GetSurface(1));
         }
 
-        void Main()
+        private void DisplayCargoFillLevel(IMyTextSurface Screen)
         {
-            DisplayFuel();
-            DisplayOres();
-            DisplayCargoFillLevel();
+            var screen = Screen;
+            if (screen != null)
+            {
+                screen.ContentType = ContentType.TEXT_AND_IMAGE;
+                screen.FontSize = 3f;
+            }
 
-        }
-        public void DisplayCargoFillLevel()
-        {
             List<IMyTerminalBlock> cargoContainers = new List<IMyTerminalBlock>();
             float fillAmount = 0;
 
@@ -90,30 +61,22 @@ namespace IngameScript
 
             string invFillLevel = new string('#', numHashes) + new string('=', numEquals);
 
+            screen.WriteText($"Fill Level:\n{fillAmount:P}\n\n{invFillLevel}", false);
+        }
 
-            IMyCockpit oCockpit = GridTerminalSystem.GetBlockWithName("Sam's Old Miner") as IMyCockpit;
-            var screen = oCockpit.GetSurface(1);
+        private void DisplayOres(IMyTextSurface Screen)
+        {
+            var screen = Screen;
             if (screen != null)
             {
                 screen.ContentType = ContentType.TEXT_AND_IMAGE;
                 screen.FontSize = 3f;
-                screen.WriteText($"Fill Level:\n{fillAmount:P}\n\n{invFillLevel}", false);
             }
-        }
 
-        public void DisplayOres()
-        {
             List<IMyTerminalBlock> cargoContainers = new List<IMyTerminalBlock>();
             List<MyInventoryItem> inventoryItems = new List<MyInventoryItem>();
 
             IMyCockpit oCockpit = GridTerminalSystem.GetBlockWithName("Sam's Old Miner") as IMyCockpit;
-            var screen = oCockpit.GetSurface(0);
-
-            if (screen == null)
-            {
-                Echo("Error, no cockpit screen found");
-                return;
-            }
 
             GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(cargoContainers, c => c.CustomName.Contains("Cargo Container"));
 
@@ -132,23 +95,25 @@ namespace IngameScript
                 }
             }
 
-
             StringBuilder sb = new StringBuilder("Ores:\n");
 
             foreach (var item in inventoryItems) { sb.AppendLine($" {Math.Round((double)item.Amount)} {item.Type.SubtypeId}"); }
 
-
-
             screen.FontSize = 3f;
             screen.WriteText(sb.ToString(), false);
-            
         }
-        public void DisplayFuel()
+
+        private void DisplayFuel(IMyTextSurface Screen)
         {
+            var screen = Screen;
+            if (screen != null)
+            {
+                screen.ContentType = ContentType.TEXT_AND_IMAGE;
+                screen.FontSize = 3f;
+            }
+
             List<IMyGasTank> gasTanks = new List<IMyGasTank>();
             GridTerminalSystem.GetBlocksOfType<IMyGasTank>(gasTanks, c => c.CustomName.Contains("Hydrogen Tank"));
-
-
 
             double totalCapacity = 0;
             double totalFillLevel = 0;
@@ -160,7 +125,6 @@ namespace IngameScript
                 totalFillLevel += tankFill * capacity;
             }
 
-
             double fillAmount = totalFillLevel / totalCapacity;
 
             int numHashes = (int)(fillAmount * 10);
@@ -168,18 +132,9 @@ namespace IngameScript
 
             string fillLevel = new string('#', numHashes) + new string('=', numEquals);
 
-
             IMyCockpit oCockpit = GridTerminalSystem.GetBlockWithName("Sam's Old Miner") as IMyCockpit;
-            var screen = oCockpit.GetSurface(2);
 
-            if (screen != null)
-            {
-                screen.ContentType = ContentType.TEXT_AND_IMAGE;
-                screen.FontSize = 3f;
-                screen.WriteText($"Tank Level:\n{fillAmount:P}\n\n{fillLevel}", false);
-            }
+            screen.WriteText($"Tank Level:\n{fillAmount:P}\n\n{fillLevel}", false);
         }
-
-
     }
 }
